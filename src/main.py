c6661f3d5c0420255abd0d72de61e09063cd13eb
sasumaki <sasu.makinen@helsinki.fi>
2019-02-03 16:18:22 +0200

    parsing users based on dates to dataframe pickle

--- src/main.py
+++ src/main.py
@@ -1,21 +1,40 @@
 
 import pandas as pd
 import networkx as nx
-import xml.etree.ElementTree as ET
+from lxml import etree
 import numpy as np
+from datetime import datetime
 
-def iter_docs(author):
-  print(author)
-  author_attr = author.attrib
-  for doc in author.iter('document'):
-    doc_dict = author_attr.copy()
-    doc_dict.update(doc.attrib)
-    doc_dict['data'] = doc.text
-    yield doc_dict
+def parse_users(min_date, max_date):
+  users_xml_items = ["Id", "Reputation", "CreationDate",
+   "DisplayName", "LastAccessDate", "WebsiteUrl", "Location", "AboutMe",
+    "Views", "UpVotes", "DownVotes", "ProfileImageUrl", "AccountId"]
+
+  maxtime = datetime.strptime(max_date, "%Y-%m-%dT%H:%M:%S.%f")
+  mintime = datetime.strptime(min_date, "%Y-%m-%dT%H:%M:%S.%f")
+
+  df = pd.DataFrame(columns=users_xml_items)
+  i= 0
+  rows = list()
+  for event, element in etree.iterparse("./data/Users.xml"):
+    if i % 1000000 == 0:
+      print(i,len(rows))
+    try:
+      time = datetime.strptime(element.items()[2][1], "%Y-%m-%dT%H:%M:%S.%f")
+      if ( time > mintime  and time < maxtime):
+        rows.append(element.items())
+    except:
+      print(element.items())
+    i = i + 1
+    element.clear()
+
+  df = pd.DataFrame([[v for k,v in r] for r in rows], columns=users_xml_items)
+  df.to_pickle("data/Users.pkl")
+  return df
         
 
 if __name__ == "__main__":
   print("parsing")
-  etree = ET.parse("./data/stackoverflow.com-Badges/Badges.xml")
-  print(etree)
-  doc_df = pd.DataFrame(list(iter_docs(etree.getroot())))
+  #df = pd.read_pickle("data/Users.pkl")
+  #print(df)
+  parse_users('2014-01-01T00:00:00.000', '2016-01-01T00:00:00.000')
